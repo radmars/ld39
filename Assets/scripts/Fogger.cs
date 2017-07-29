5283e56d41c998f31b06ed2f31432d387358fc57
Eugene Marcotte <emarcotte@gmail.com>
2017-07-29 14:41:05 -0400

    trash mode engaged

---
+++ Assets/scripts/Fogger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fogger : MonoBehaviour {
+    public float density = .15f;
+    public float start = 20;
+    public float end = 180;
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Fog it up");
+        RenderSettings.fog = true;
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogColor = new Color(0xdf, 0xa5, 0x00);
+    }
+
+    public void Update()
+    {
+        RenderSettings.fogStartDistance = start;
+        RenderSettings.fogEndDistance = end;
+        RenderSettings.fogDensity = density;
+	}
+}
