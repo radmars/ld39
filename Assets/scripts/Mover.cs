d30983dee486a62f7fc8050f821a5d82ecd07423
Eugene Marcotte <emarcotte@gmail.com>
2017-07-29 09:59:25 -0400

    Can move forwards/backwards and collide with the first grid.

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    float forward;
+    float sideways;
+    private Rigidbody body;
+    public float speed = 1.0f;
+
+    private void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	private void Update () {
+        forward = Input.GetAxis("Vertical");
+        sideways = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = transform.forward * forward * speed * Time.deltaTime;
+        Debug.Log("" + transform.forward + " " + forward+ " " +speed + " " + Time.deltaTime);
+
+        // Apply this movement to the rigidbody's position.
+        body.MovePosition(body.position + movement);
+    }
+}
