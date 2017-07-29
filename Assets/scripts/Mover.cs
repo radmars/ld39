0acf012c0839d04692c6e8d798fca41360b53cd0
Eugene Marcotte <emarcotte@gmail.com>
2017-07-29 11:37:14 -0400

    random spawns semi work

--- Assets/scripts/Mover.cs
+++ Assets/scripts/Mover.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Mover : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
 
     float forward;
     float sideways;
     private Rigidbody body;
     public float speed = 1.0f;
 
-    private void Start () {
+    private void Start()
+    {
         body = GetComponent<Rigidbody>();
-	}
-	
-	private void Update () {
+    }
+
+    private void Update()
+    {
         forward = Input.GetAxis("Vertical");
         sideways = Input.GetAxis("Horizontal");
     }
@@ -21,9 +22,6 @@ public class Mover : MonoBehaviour {
     private void FixedUpdate()
     {
         Vector3 movement = (transform.right * sideways + transform.forward * forward).normalized * speed * Time.deltaTime;
-        Debug.Log("" + transform.forward + " " + forward+ " " +speed + " " + Time.deltaTime);
-
-        // Apply this movement to the rigidbody's position.
         body.MovePosition(body.position + movement);
     }
 }
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    Mover rover;
+    float lastSpawnTime;
+    public Critter[] critterPrefabs;
+
+    void Start()
+    {
+        rover = FindObjectOfType<Mover>();
+    }
+
+    bool ShouldSpawn(float lastSpawnTime, float now)
+    {
+        return (now - lastSpawnTime > 5)
+            && (Random.Range(0, 10) > 1);
+    }
+
+    void FixedUpdate()
+    {
+        if (ShouldSpawn(lastSpawnTime, Time.time))
+        {
+        Debug.Log("Check check?");
+            lastSpawnTime = Time.time;
+            SpawnACritter();
+        }
+    }
+
+    void SpawnACritter()
+    {
+        // Pick a random place around the rover
+        var around = Random.insideUnitCircle.normalized;
+        // Offset from the player
+        var offset = rover.transform.position + new Vector3(around.x, around.y);
+        // Scale to a range away from the player 
+        offset *= Random.Range(20, 40);
+
+        // Scan for terrain
+        var heightTestRay = new Ray(
+            new Vector3(offset.x, offset.y) + new Vector3(0, 100),
+            Vector3.down
+        );
+
+        RaycastHit collision;
+        if (Physics.Raycast(heightTestRay, out collision, float.MaxValue, LayerMask.GetMask("Terrain")))
+        {
+            var prefab = critterPrefabs[Random.Range(0, critterPrefabs.Length)];
+            var instance = Instantiate(prefab);
+            instance.transform.position = collision.point;
+        }
+    }
+}
