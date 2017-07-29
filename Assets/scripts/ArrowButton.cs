40f43ba02f63aa299075996cda0f940981c0a370
Eugene Marcotte <emarcotte@gmail.com>
2017-07-29 19:07:16 -0400

    Shot selector screen

---
+++ Assets/scripts/ArrowButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowButton : MonoBehaviour
+{
+
+    private Sprite self;
+    public Sprite flash;
+    private SpriteRenderer spriteRenderer;
+    private int flashid;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        self = spriteRenderer.sprite;
+    }
+
+
+    internal void Flash()
+    {
+        StartCoroutine(_Flash());
+    }
+
+    private IEnumerator _Flash()
+    {
+        spriteRenderer.sprite = flash;
+        var i = ++flashid;
+        yield return new WaitForSeconds(0.05f);
+        if (flashid == i)
+        {
+            spriteRenderer.sprite = self;
+        }
+    }
+}
