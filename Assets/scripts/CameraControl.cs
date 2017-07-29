ddea1f79ea2abf8c6e36f322f6868cd357011f33
Erik Driscoll <edriscoll@softheon.com>
2017-07-29 11:03:21 -0400

    Added camera controls script and script to rover

---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class for controling the camera 
+/// </summary>
+public class CameraControl : MonoBehaviour
+{
+    /// <summary>
+    /// Bool to set showing mouse cursor
+    /// </summary>
+    public bool ShowCurser;
+
+    /// <summary>
+    /// The sensitivity
+    /// </summary>
+    public float Sensitivity;
+
+    /// <summary>
+    /// The start method
+    /// </summary>
+    void Start()
+    {
+        if (!ShowCurser)
+        {
+            Cursor.visible = false;
+        }
+    }
+
+    /// <summary>
+    /// Update method
+    /// </summary>
+    void Update()
+    {
+        float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
+        float newRotationX = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * Sensitivity;
+
+        gameObject.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
+    }
+}
