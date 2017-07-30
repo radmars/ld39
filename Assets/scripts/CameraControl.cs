9881396a50303438da550999b3da8a9d2bf52438
Erik Driscoll <edriscoll@softheon.com>
2017-07-30 00:55:45 -0400

    Updated rails to allow to be circular, go forwards then backwards (patrol) as well as choose between once and loop

--- Assets/scripts/CameraControl.cs
+++ Assets/scripts/CameraControl.cs
@@ -24,31 +24,17 @@ public class CameraControl : MonoBehaviour
         if (!ShowCurser)
         {
             Cursor.visible = false;
-        }
+        }        
     }
-
-    private void Awake()
-    {
-        var camera = GetComponent<Camera>();
-        camera.targetTexture.width = 320;
-        camera.targetTexture.height= 240;
-    }
-
+    
     /// <summary>
     /// Update method
     /// </summary>
     void Update()
     {
-        float newRotationY = transform.localEulerAngles.y + GetFurthestAxis("Horizontal", "Mouse X") * Sensitivity;
-        float newRotationX = transform.localEulerAngles.x - GetFurthestAxis("Vertical", "Mouse Y") * Sensitivity;
+        float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
+        float newRotationX = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * Sensitivity;
 
         gameObject.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
-    }
-
-    float GetFurthestAxis(string a, string b)
-    {
-        float aa = Input.GetAxis(a);
-        float ba = Input.GetAxis(b);
-        return (Mathf.Abs(aa) < Mathf.Abs(ba)) ?  ba: aa;
     }
 }
--- Assets/scripts/RailMover.cs
+++ Assets/scripts/RailMover.cs
@@ -4,46 +4,95 @@ using System.Collections;
 public class RailMover : MonoBehaviour
 {
     public Rail rail;
+
     public PlayMode mode;
+
+    public bool isReversed;
+
+    public bool isLooping;
+
+    public bool returns;
 
     public float speed = 2.5f;
 
     private int currentSeg;
+
     private float transition;
-    
+
     private bool isCompleted;
 
-    
+
 
     private void Update()
     {
-        if(!rail)
-        return;
+        if (!rail)
+            return;
 
-        if(!isCompleted)
-        Play();
+        if (!isCompleted)
+            Play(!isReversed);
     }
 
-    private void Play()
+    private void Play(bool forward = true)
     {
+        //calculate speed and stuff
         float m = (rail.nodes[currentSeg + 1].position - rail.nodes[currentSeg].position).magnitude;
         float s = (Time.deltaTime * 1 / m) * speed;
-        transition += s;
-        if(transition >1)
+        transition += (forward) ? s : -s;
+        //check transition
+        if (transition > 1)
         {
-            transition=0;
+            transition = 0;
             currentSeg++;
-
-            if (currentSeg == rail.nodes.Length-1)
+            //if we reached the end
+            if (currentSeg == rail.nodes.Length - 1)
             {
-                isCompleted = true;
-                return;
+                //if we're looping
+                if (isLooping)
+                {
+                    //if we comin home bby
+                    if(returns)
+                    {
+                        transition = 1;
+                        currentSeg = rail.nodes.Length -2;
+                        isReversed = !isReversed;
+                    }
+                    else{
+                        currentSeg=0;
+                    }
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+
             }
         }
-        else if(transition<0)
+        else if (transition < 0)
         {
-            transition=1;
+            transition = 1;
             currentSeg--;
+
+            if (currentSeg == -1)
+            {
+                if (isLooping)
+                {
+                    if(returns)
+                    {
+                        transition = 0;
+                        currentSeg = 0;
+                        isReversed = !isReversed;
+                    }
+                    else{
+                        currentSeg= rail.nodes.Length-2;
+                    }
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+            }
         }
 
         transform.position = rail.PositionOnRail(currentSeg, transition, mode);
