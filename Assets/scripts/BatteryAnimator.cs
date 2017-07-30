a9f4e8c12b9ca570e62ca35ad3d4b4fced04d5c6
Eugene Marcotte <emarcotte@gmail.com>
2017-07-30 12:37:10 -0400

    A REALLY GOOD BATTERY UI

---
+++ Assets/scripts/BatteryAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BatteryAnimator : MonoBehaviour
+{
+
+    private SpriteRenderer r;
+    public Sprite[] sprites;
+    public float timeToDrain = 60 * 5;
+    public float startTime = 0;
+
+    public void Start()
+    {
+        startTime = Time.fixedTime;
+        r = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        int spriteIndex = sprites.Length - (int)((Time.fixedTime - startTime) / timeToDrain * sprites.Length) - 1;
+        if (spriteIndex >= 0)
+        {
+            r.sprite = sprites[spriteIndex];
+        }
+    }
+}
