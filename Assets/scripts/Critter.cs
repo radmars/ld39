079842684ce248db9dc52e15aa28448f6305364e
Erik Driscoll <edriscoll@softheon.com>
2017-07-29 21:38:23 -0400

    push to critter so take picture changes work

--- Assets/scripts/Critter.cs
+++ Assets/scripts/Critter.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Critter : MonoBehaviour
 {
     public string name;
     public double points;
+
+    public double DistanceModifier = .02;
+
+    public double FacingModifier = 5;
     private void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
@@ -27,13 +32,26 @@ public class Critter : MonoBehaviour
     {
         var picValue = points;
 
-       var distance = Vector3.Distance(transform.position, player.transform.position);
-        
-        Debug.Log(distance);
-        
-           
-            picValue = picValue * (.02 * (100 - distance));
-        
+        //Distance from player
+        var distance = Vector3.Distance(transform.position, player.transform.position);
+       // Debug.Log("the distance is :" + distance);
+        if (distance < 25)
+        {
+            picValue = picValue * (DistanceModifier * (100 - distance));
+        }
+
+        //how close they are to facing ecahother
+
+        //float facing = Math.Abs(Vector3.Angle(transform.forward, player.transform.position));
+        //Debug.Log("Facing eachother (the closer to 0 the better)? :" + facing);
+        //picValue = picValue * FacingModifier * (1 - facing);
+
+        float facing = Math.Abs(Vector3.Dot(transform.forward, player.transform.position));
+        //Debug.Log("Facing eachother (the closer to 0 the better)? :" + facing);
+        picValue = picValue * FacingModifier * (1 - facing);
+
+
+
         return picValue;
     }
 }
