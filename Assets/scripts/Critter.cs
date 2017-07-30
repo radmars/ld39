9a7a210aa9a72381de843688f78f08f7432198ea
Erik Driscoll <edriscoll@softheon.com>
2017-07-29 23:27:47 -0400

    Updated score values to be better ~(0-250) points per picture

--- Assets/scripts/Critter.cs
+++ Assets/scripts/Critter.cs
@@ -36,23 +36,21 @@ public class Critter : MonoBehaviour
 
         //Distance from player
         var distance = Vector3.Distance(transform.position, player.transform.position);
-        Debug.Log("points for distance :" + (points * (DistanceModifier * (100 - distance))));
+        //Debug.Log("points for distance :" + (points * (DistanceModifier * (1 - Mathf.Clamp(distance, 0, 40)/20))));
         if (distance < 25)
         {
-            picValue = picValue + (points * (DistanceModifier * (100 - distance)));
+            picValue = picValue + (points * (DistanceModifier * (1 - Mathf.Clamp(distance, 0, 40)/20)));
         }
-
+        
         //how close to center of the screen
         float center = Math.Abs(Vector3.Angle(player.transform.forward, transform.position - player.transform.position));
-        Debug.Log("Points for Angle off from center of screen (the closer to 0 the better) :" + (points * CenterModifier * (90 - center)));
-        picValue = picValue + (points * CenterModifier * (90 - center));
+        //Debug.Log("Points for Angle off from center of screen:" + (points * CenterModifier * (90 - Mathf.Clamp(center, 0, 90))/90));
+        picValue = picValue + (points * CenterModifier * (1 - Mathf.Clamp(center, 0 , 90))/90);
 
         //How close you are to facing eachother.
         float facing = Math.Abs(Vector3.Dot(player.transform.forward, transform.forward));
-        Debug.Log("Points for facing eachother (the closer to 0 the better)? :" + (points * FacingModifier * (1 - facing)));
-        picValue = picValue + (points * FacingModifier * (1 - facing));
-
-
+        //Debug.Log("Points for facing eachother:" + (points * FacingModifier * (1 - facing)));
+        picValue = picValue + (points * FacingModifier * (1 - facing*2));
 
         return picValue;
     }
